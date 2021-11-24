c771b1b0d36996ee9426e0098a41e798b1a02283
Vinicius Rodrigues <vrsstudy@gmail.com>
2021-11-24 18:38:06 -0400

    charts

--- master/assets/js/chart.js
+++ master/assets/js/chart.js
@@ -42,13 +42,91 @@ const getTransactions = async () => {
   }
 }
 
+const getCosts = async() => {
+  try{
+    const response = await axios.get(`${baseURL}/transaction/costs`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const values = response.data.map(res => Number(res.sum))
+    options2.series[0].data = values
+
+    
+    const categories = response.data.map(res => res.name)
+    options2.xaxis.categories = categories
+
+    var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+    chart2.render();
+    
+    
+  }catch(err){
+    console.log('opa deu merda')
+  }
+}
+
+const getPayments = async() => {
+  try{
+    const response = await axios.get(`${baseURL}/transaction/payments`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const values = response.data.map(res => Number(res.sum))
+    options3.series = values
+
+    
+    const categories = response.data.map(res => res.name)
+    options3.labels = categories
+
+    var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+    chart3.render();
+    
+    
+  }catch(err){
+    console.log('opa deu merda')
+  }
+}
+
+const getCostTypes = async () => {
+  try {
+    const response = await axios.get(`${baseURL}/costs`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    response.data.map(res => {
+      document.querySelector('#cost_type').insertAdjacentHTML('beforeend', `<option value="${res.id}">${res.name}</option>`)
+    })
+  }catch(err){
+
+  }      
+}
+
+const getPaymentTypes = async () => {
+  try{
+    const response = await axios.get(`${baseURL}/payment`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    response.data.map(res => {
+      document.querySelector('#payment_type').insertAdjacentHTML('beforeend', `<option value="${res.id}">${res.name}</option>`)
+    })
+  }catch(err){
+
+  }   
+}
+
 var options = {
   series: [{
     name: 'Entradas',
-    data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+    data: [3800]
   }, {
     name: 'Saidas',
-    data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    data: [800, 700, 250, 400, 120, 350]
   }],
   chart: {
     type: 'bar',
@@ -70,7 +148,7 @@ var options = {
     colors: ['transparent']
   },
   xaxis: {
-    categories: [],
+    categories: ['10', '11', '12', '13', '14', '15'],
   },
   yaxis: {
     title: {
@@ -83,24 +161,73 @@ var options = {
   tooltip: {
     y: {
       formatter: function (val) {
-        return "R$ " + val
+        return ("R$ " + val + '\n')
       }
     }
   }
 };
 
-getTransactions()
+var options2 = {
+  series: [{
+    data: []
+  }],
+  labels: ['Moradia', 'Alimentação', 'Lazer', 'Energia', 'Água', 'Outros'],
+  chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['Moradia', 'Alimentação', 'Lazer', 'Energia', 'Água', 'Outros'],
+  }
+};
+
+var options3 = {
+  series: [14, 23, 21, 17, 15],
+  chart: {
+    type: 'polarArea',
+  },
+  labels: ['Débito', 'Crédito', 'Ted/Doc', 'Cheque', '    PIX    '],
+  stroke: {
+    colors: ['#fff']
+  },
+  fill: {
+    opacity: 0.8
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+};
 
 const newTransaction = async (e) => {
   e.preventDefault
   const value = document.querySelector("#value").value
   const type = document.querySelector("#select").value
-  if (value && type) {
+  const description = document.querySelector('#description').value
+  const cost_type = document.querySelector('#cost_type').value
+  const payment_type = document.querySelector('#payment_type').value
+  if (value && type && description && cost_type && payment_type) {
     try {
 
       await axios.post(`${baseURL}/transaction`, {
         value: value,
         type: type,
+        description: description,
+        cost_id: cost_type,
+        payment_id: payment_type,
         date: new Date(Date.now())
       }, {
         headers: {
@@ -132,9 +259,10 @@ const newTransaction = async (e) => {
 const logout = () => {
   localStorage.removeItem('token')
   localStorage.removeItem('user')
-
   window.location.href = '/gerenciador-de-gastos/login.html'
 }
+
+
 
 
 document.getElementById('newTransaction').addEventListener(
@@ -145,4 +273,9 @@ document.getElementById('logout').addEventListener(
   'click', logout, false
 );
 
+getPaymentTypes()
+getCostTypes()
+getTransactions()
+getCosts()
+getPayments()
 
